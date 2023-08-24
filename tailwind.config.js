dc3788784d09a371ba7508dc4cd47e1d58784ae5
HienCao <caohiendev01@gmail.com>
2023-08-24 09:56:08 +0700

    form v1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,css,scss,js}"],
+  theme: {
+    extend: {
+      width: {
+        '420' : '420px'
+      },
+    },
+  },
+  plugins: [],
+  
+}
+
